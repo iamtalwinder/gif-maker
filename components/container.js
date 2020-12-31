4b73d5fef4a9fbea84861dcb130d94cc1d60a8e8
Talwinder Singh <singhtalwinder790@gmail.com>
2020-12-31 10:35:06 +0530

    :package: Add container component

---
+++ components/container.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 350px;
+  max-width: 700px;
+  margin: auto;
+  border: 1px solid #ccc;
+`;
+
+export default Container;
