481b93f13fb728034bcf83b00c7407766b85f804
Talwinder Singh <singhtalwinder790@gmail.com>
2020-12-31 13:45:12 +0530

    :lipstick Add visible props in container

--- components/container.js
+++ components/container.js
@@ -2,11 +2,28 @@ import styled from "styled-components";
 
 const Container = styled.div`
   transition: 0.5s ease;
-  width: 100%;
-  height: 350px;
   max-width: 700px;
   margin: auto;
   border: 1px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  opacity: 0;
+  pointer-events: none;
+  width: 0;
+  height: 0;
+
+  ${(props) =>
+    props.visible &&
+    `
+    width: 100%;
+    height: 400px;
+    max-width: 700px;
+    opacity: 1;
+    pointer-events: auto;
+    `}
 `;
 
 export default Container;
