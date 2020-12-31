aeb831afde657f9afb3175ffd03744b019a48120
Talwinder Singh <singhtalwinder790@gmail.com>
2020-12-31 13:59:52 +0530

    :package: Add logo component

---
+++ components/logo.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+`;
+
+const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-right: 10px;
+`;
+
+export default function Logo({ text, className }) {
+  return (
+    <Container className={className}>
+      <Img src="/logo.png" />
+      <span>{text}</span>
+    </Container>
+  );
+}
